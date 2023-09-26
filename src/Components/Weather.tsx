85018370c22beed52c26f43af9e14774b7914b7a
MatheusLucasAndrade <theuslucasandrade@gmail.com>
2023-09-25 22:55:10 -0300

    updated weather component, fix some bugs

--- src/Components/Weather.tsx
+++ src/Components/Weather.tsx
@@ -4,16 +4,11 @@ import { icons } from "../assets/icons/Icons";
 import Loading from "./Loading";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import snakeToCamelCase from "../Common/Snake2Camel";
+import React from "react";
 
 const apiUrl = import.meta.env.VITE_REACT_APP_WEATHER_API_URL_CURRENT;
 
 interface IDataWeather {
-  location: {
-    name: string;
-    region: string;
-    country: string;
-    localtime: string;
-  };
   current: {
     condition: {
       code: number;
@@ -21,34 +16,54 @@ interface IDataWeather {
     };
     lastUpdated: string;
     tempC: number;
-    isDay: number;
-    windKph: number;
-    windDegree: number;
-    windDir: string;
-    pressureMb: number;
-    pressureIn: number;
-    precipMm: number;
-    precipIn: number;
-    humidity: number;
-    cloud: number;
-    feelslikeC: number;
-    visKm: number;
-    uv: number;
-    gustKph: number;
   };
+  forecast: {
+    forecastday: {
+      0: {
+        day: {
+          dailyChanceOfRain: number;
+          maxtempC: number;
+          mintempC: number;
+        };
+      };
+    };
+  };
+}
+
+interface IDayNight {
+  sunriseHour: number;
+  sunsetHour: number;
 }
 
 const url = apiUrl;
 
-const wind = <FontAwesomeIcon icon={icons.wind} beat />;
-const temp = <FontAwesomeIcon icon={icons.temperatureThreeQuarters} beat />;
-const prec = <FontAwesomeIcon icon={icons.cloudShowersHeavy} beat />;
-const radi = <FontAwesomeIcon icon={icons.radiation} beat />;
-const drop = <FontAwesomeIcon icon={icons.droplet} beat />;
+const day = <FontAwesomeIcon className="color-sun" icon={icons.sun} />;
+const night = <FontAwesomeIcon className="color-light-sun" icon={icons.moon} />;
+const cloudDay = (
+  <FontAwesomeIcon className="color-light-sun" icon={icons.cloudSun} />
+);
+const cloudNight = (
+  <FontAwesomeIcon className="color-light-night" icon={icons.cloudMoon} />
+);
+const dayRain = (
+  <FontAwesomeIcon className="color-light-night" icon={icons.cloudSunRain} />
+);
+const nightRain = (
+  <FontAwesomeIcon className="color-light-night" icon={icons.cloudMoonRain} />
+);
 
 const Weather = () => {
   const [values, setValues] = useState<IDataWeather>();
+  const [iconWeather, setIconWeather] = useState<React.JSX.Element>(
+    <FontAwesomeIcon icon={icons.default} spin />
+  );
   const { data, loading } = useFetch<IDataWeather>(url);
+
+  const isDay = ({ sunriseHour, sunsetHour }: IDayNight): boolean => {
+    const now = new Date();
+    const currentHour = now.getHours();
+    return currentHour >= sunriseHour && currentHour < sunsetHour;
+  };
 
   useEffect(() => {
     if (data) {
@@ -56,38 +71,51 @@ const Weather = () => {
     }
   }, [data]);
 
+  useEffect(() => {
+    const changeIcon = (code: number) => {
+      const current = isDay({ sunriseHour: 6, sunsetHour: 18 });
+      let icon: React.JSX.Element | undefined;
+
+      if (code === 1000 && current) {
+        icon = day;
+      } else if (code === 1000 && !current) {
+        icon = night;
+        console.log("chamou aquiii");
+      } else if (code > 1000 && code <= 1030 && current) {
+        icon = cloudDay;
+      } else if (code > 1000 && code <= 1030 && !current) {
+        icon = cloudNight;
+      } else if (code > 1030 && current) {
+        icon = dayRain;
+      } else if (code > 1030 && !current) {
+        icon = nightRain;
+      }
+
+      return icon;
+    };
+
+    if (values) {
+      const icon = changeIcon(values.current.condition.code);
+      setIconWeather(icon ?? <FontAwesomeIcon icon={icons.default} spin />);
+    }
+  }, [values]);
+
   if (loading) {
     return <Loading />;
   }
 
-  console.log(data?.current.condition.code);
-  const { current, location } = values ?? {};
+  const { current, forecast } = values ?? {};
 
   return (
-    <div className="">
-      <div className="text-center font-size-4 bold mb">{location?.name}</div>
-      <ul className="font-size-2">
-        <li className="mb-s">
-          <span>{wind}</span>
-          {current?.windDir} - {current?.windKph} Km/h
-        </li>
-        <li className="mb-s">
-          <span>{temp}</span>
-          {current?.tempC}Cº - {current?.feelslikeC}Cº
-        </li>
-        <li className="mb-s">
-          <span>{drop}</span>
-          {current?.humidity}%
-        </li>
-        <li className="mb-s">
-          <span>{prec}</span>
-          {current?.precipIn}%
-        </li>
-        <li className="mb-s">
-          <span>{radi}</span>
-          {current?.uv}
-        </li>
-      </ul>
+    <div>
+      <div className="flex">
+        <span className="font-size-8">{iconWeather}</span>
+        <div className="font-size-2 temperature">
+          <span>MIN: {forecast?.forecastday[0].day.mintempC}Cº</span>
+          <span>MAX: {forecast?.forecastday[0].day.maxtempC}Cº</span>
+          <span className="color-sun font-size-3 bold">{current?.tempC}Cº</span>
+        </div>
+      </div>
     </div>
   );
 };
